bc84177fd8af4f513079549ad917cea854f1e842
Skuznetz <s.kuznetzov@mail.ru>
2017-10-04 20:58:46 +0300

    новый маршрут

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+  
+  authenticated :user do
+    root 'projects#index', as: :authenticated_root
+  end
+
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
