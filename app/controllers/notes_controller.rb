fd975cd442937ff9c37ae9ee0d36c645a9e85a65
Skuznetz <s.kuznetzov@mail.ru>
2017-10-12 15:13:54 +0300

    ставим фильтры

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  before_action :set_project
+  before_action :project_owner?
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   # GET /notes
