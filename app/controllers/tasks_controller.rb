c475ee8bb8a4538f051bf5da4e9c07241053df5c
Skuznetz <s.kuznetzov@mail.ru>
2017-10-16 17:02:08 +0300

    метод toggle

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,6 +63,10 @@ class TasksController < ApplicationController
     end
   end
 
+   def toggle
+    @task.update_attributes(completed: !@task.completed)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
