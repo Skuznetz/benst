8d0bc8ca859e161994c4c52db2d9b72928b23c64
Skuznetz <s.kuznetzov@mail.ru>
2017-10-18 21:30:28 +0300

    обновляем маршруты

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :tasks
   devise_for :users, controllers: { registrations: 'registrations' }
 
@@ -14,8 +15,7 @@ Rails.application.routes.draw do
       end
     end
   end
-
   
+ root "home#index"
 
-  root "home#index"
 end
