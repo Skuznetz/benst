ce50cf0dfb2f657ee549c2d79f9c7ff9bb7430dc
Skuznetz <s.kuznetzov@mail.ru>
2017-10-22 18:18:57 +0300

    ставим модуль API

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,74 +1,43 @@
-class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+module Api
+  class ProjectsController < ApplicationController
 
-  # GET /projects
-  # GET /projects.json
-  def index
-    @projects = current_user.projects
-  end
+    prepend_before_action :authenticate_user_from_token!
 
-  # GET /projects/1
-  # GET /projects/1.json
-  def show
-  end
-
-  # GET /projects/new
-  def new
-    @project = Project.new
-  end
-
-  # GET /projects/1/edit
-  def edit
-  end
-
-  # POST /projects
-  # POST /projects.json
-  def create
-    @project = current_user.projects.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    def index
+      @projects = current_user.projects
+      render json: @projects
     end
-  end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
-  def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /projects/1
-  # DELETE /projects/1.json
-  def destroy
-    @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
+    def show
       @project = Project.find(params[:id])
+      render json: @project
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def create
+      @project = current_user.projects.new(project_params)
+
+      if @project.save
+        render json: { status: :created }
+      else
+        render json: @project.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def authenticate_user_from_token!
+      user_email = params[:user_email].presence
+      user = user_email && User.find_by(email: user_email)
+      if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+        sign_in user, store: true
+      else
+        render json: { status: "auth failed" }
+        false
+      end
+    end
+
     def project_params
       params.require(:project).permit(:name, :description, :due_on)
     end
+  end
 end
